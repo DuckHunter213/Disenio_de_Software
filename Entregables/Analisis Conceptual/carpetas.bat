d5a732051fc40d3c701898b48d45a26f251aa871
Fercho <gomez.alejandre@gmail.com>
2016-12-05 18:03:06 -0600

    Commit Super-Maestro

---
+++ Entregables/Analisis Conceptual/carpetas.bat
@@ -0,0 +1,9 @@
+mkdir administrar devoluciones
+mkdir administrar material
+mkdir administrar multas
+mkdir administrar préstamos
+mkdir administrar reservados
+mkdir administrar usuario
+mkdir iconosBiblioteca
+mkdir inicio sesion
+mkdir renovar usuario
