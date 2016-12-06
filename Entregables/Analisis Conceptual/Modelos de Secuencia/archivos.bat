d7cd24e414d6523b0d40e52e43e80f8f52973709
Fercho <gomez.alejandre@gmail.com>
2016-12-05 21:38:22 -0600

    Commit Final :+1:

---
+++ Entregables/Analisis Conceptual/Modelos de Secuencia/archivos.bat
@@ -0,0 +1,12 @@
+xcopy 1 > "CU1 Registrar Devolucion.bmp"
+xcopy 1 > "CU10 Reservar Items.bmp"
+xcopy 1 > "CU2 Renovar Prestamo.bmp"
+xcopy 1 > "CU3 Administrar Items.bmp"
+xcopy 1 > "CU4 Renovar Permisos.bmp"
+xcopy 1 > "CU5 Saldar Multas.bmp"
+xcopy 1 > "CU6 Administrar Usuario.bmp"
+xcopy 1 > "CU7 Realizar Prestamo.bmp"
+xcopy 1 > "CU8 Generar Reporte.bmp"
+xcopy 1 > "CU9 Retirar Permisos.bmp"
+
+pause
